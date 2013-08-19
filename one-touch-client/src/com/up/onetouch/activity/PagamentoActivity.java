720fb08ca0765bb72c62d0adf2a203e341438ca7
MXavier1980 <Marcel Xavier@MX-PC>
2013-08-19 20:57:19 -0300

    Commit Pagamento

---
+++ one-touch-client/src/com/up/onetouch/activity/PagamentoActivity.java
@@ -0,0 +1,315 @@
+
+package com.up.onetouch.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.up.onetouch.R;
+import com.up.onetouch.wallet.IabHelper;
+import com.up.onetouch.wallet.IabResult;
+import com.up.onetouch.wallet.Inventory;
+import com.up.onetouch.wallet.Purchase;
+
+
+
+public class PagamentoActivity extends Activity {
+
+	static final String TAG = "Pagamento One Touch";
+        
+    static final String SKU_R400 = "valor_400";
+    static final String SKU_R500 = "valor_500";
+    static final String SKU_R600 = "valor_600";
+        
+    // (arbitrary) request code for the purchase flow
+    static final int RC_REQUEST = 10400;
+
+    IabHelper mHelper;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* base64EncodedPublicKey should be YOUR APPLICATION'S PUBLIC KEY
+         * (that you got from the Google Play developer console). This is not your
+         * developer public key, it's the *app-specific* public key.
+         *
+         * Instead of just storing the entire literal string here embedded in the
+         * program,  construct the key at runtime from pieces or
+         * use bit manipulation (for example, XOR with some other string) to hide
+         * the actual key.  The key itself is not secret information, but we don't
+         * want to make it easy for an attacker to replace the public key with one
+         * of their own and then fake messages from the server.
+         */
+        String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkXbG+2k0+xmKm2eqZbcBvqTwRsH9T6v3f6wzk/N6ybVfyUINVxyJB5bs29nZ6Uqest5UpJxkLhFMB0iUfchN81ywU/xsDC0cp3wWaUJVBINmClnFVmyWogzMmwkBXX2G6wcybCxYKcTnVQC1uO7qqdj+oD0RAxQ4O1IdZGFL4AAAqo8TFetZKJwbQP00pro383kHPGyP8kOCzzOXD+GsTQjU2KJef/3PaOCLofAm2P8ICAFvGoUyxBbZQubp3cAWXlDaFWe8iLhVFhaavRQBOEbrmRNv3MDMadgjY07R9YAl4wX8KdQj+olmmUTfY/9EJW4nUfgvbCNijerIBA97RQIDAQAB";
+        
+        // Some sanity checks to see if the developer (that's you!) really followed the
+        // instructions to run this sample (don't put these checks on your app!)
+        if (base64EncodedPublicKey.contains("CONSTRUCT_YOUR")) {
+            throw new RuntimeException("Please put your app's public key in MainActivity.java. See README.");
+        }
+        if (getPackageName().startsWith("com.example")) {
+            throw new RuntimeException("Please change the sample's package name! See README.");
+        }
+        
+        // Create the helper, passing it our context and the public key to verify signatures with
+        Log.d(TAG, "Creating IAB helper.");
+        mHelper = new IabHelper(this, base64EncodedPublicKey);
+        
+        // enable debug logging (for a production application, you should set this to false).
+        mHelper.enableDebugLogging(true);
+
+        // Start setup. This is asynchronous and the specified listener
+        // will be called once setup completes.
+        Log.d(TAG, "Starting setup.");
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                Log.d(TAG, "Setup finished.");
+
+                if (!result.isSuccess()) {
+                    // Oh noes, there was a problem.
+                    complain("Problem setting up in-app billing: " + result);
+                    return;
+                }
+
+                // Hooray, IAB is fully set up. Now, let's get an inventory of stuff we own.
+                Log.d(TAG, "Setup successful. Querying inventory.");
+                mHelper.queryInventoryAsync(mGotInventoryListener);
+            }
+        });
+    }
+
+    // Listener that's called when we finish querying the items and subscriptions we own
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory) {
+            Log.d(TAG, "Query inventory finished.");
+            if (result.isFailure()) {
+                complain("Failed to query inventory: " + result);
+                return;
+            }
+
+            Log.d(TAG, "Query inventory was successful.");
+            
+            
+            Purchase r400Purchase = inventory.getPurchase(SKU_R400);
+            setWaitScreen(false);
+            Log.d(TAG, "Initial inventory query finished; enabling main UI.");
+        }
+    };
+
+    // User clicked the "Pagar" button
+    public void pagar400(View arg0) {
+        Log.d(TAG, "Pagar 4,00 button clicked.");
+
+        setWaitScreen(true);
+        Log.d(TAG, "Iniciando pagamento do estacionamento.");
+        
+        /* TODO: for security, generate your payload here for verification. See the comments on 
+         *        verifyDeveloperPayload() for more info. Since this is a SAMPLE, we just use 
+         *        an empty string, but on a production app you should carefully generate this. */
+        String payload = ""; 
+        
+        mHelper.launchPurchaseFlow(this, SKU_R400, RC_REQUEST, 
+                mPurchaseFinishedListener, payload);
+    }
+    
+    // User clicked the "Pagar" button
+    public void pagar500(View arg0) {
+        Log.d(TAG, "Pagar 5,00 button clicked.");
+
+        setWaitScreen(true);
+        Log.d(TAG, "Iniciando pagamento do estacionamento.");
+        
+        /* TODO: for security, generate your payload here for verification. See the comments on 
+         *        verifyDeveloperPayload() for more info. Since this is a SAMPLE, we just use 
+         *        an empty string, but on a production app you should carefully generate this. */
+        String payload = ""; 
+        
+        mHelper.launchPurchaseFlow(this, SKU_R500, RC_REQUEST, 
+                mPurchaseFinishedListener, payload);
+    }
+    
+    // User clicked the "Pagar" button
+    public void pagar600(View arg0) {
+        Log.d(TAG, "Pagar 6,00 button clicked.");
+
+        setWaitScreen(true);
+        Log.d(TAG, "Iniciando pagamento do estacionamento.");
+        
+        /* TODO: for security, generate your payload here for verification. See the comments on 
+         *        verifyDeveloperPayload() for more info. Since this is a SAMPLE, we just use 
+         *        an empty string, but on a production app you should carefully generate this. */
+        String payload = ""; 
+        
+        mHelper.launchPurchaseFlow(this, SKU_R600, RC_REQUEST, 
+                mPurchaseFinishedListener, payload);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + "," + data);
+
+        // Pass on the activity result to the helper for handling
+        if (!mHelper.handleActivityResult(requestCode, resultCode, data)) {
+            // not handled, so handle it ourselves (here's where you'd
+            // perform any handling of activity results not related to in-app
+            // billing...
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+        else {
+            Log.d(TAG, "onActivityResult handled by IABUtil.");
+        }
+    }
+    
+    /** Verifies the developer payload of a purchase. */
+    boolean verifyDeveloperPayload(Purchase p) {
+        String payload = p.getDeveloperPayload();
+        
+        /*
+         * TODO: verify that the developer payload of the purchase is correct. It will be
+         * the same one that you sent when initiating the purchase.
+         * 
+         * WARNING: Locally generating a random string when starting a purchase and 
+         * verifying it here might seem like a good approach, but this will fail in the 
+         * case where the user purchases an item on one device and then uses your app on 
+         * a different device, because on the other device you will not have access to the
+         * random string you originally generated.
+         *
+         * So a good developer payload has these characteristics:
+         * 
+         * 1. If two different users purchase an item, the payload is different between them,
+         *    so that one user's purchase can't be replayed to another user.
+         * 
+         * 2. The payload must be such that you can verify it even when the app wasn't the
+         *    one who initiated the purchase flow (so that items purchased by the user on 
+         *    one device work on other devices owned by the user).
+         * 
+         * Using your own server to store and verify developer payloads across app
+         * installations is recommended.
+         */
+        
+        return true;
+    }
+
+    // Callback for when a purchase is finished
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: " + purchase);
+            if (result.isFailure()) {
+                complain("Error purchasing: " + result);
+                setWaitScreen(false);
+                return;
+            }
+            if (!verifyDeveloperPayload(purchase)) {
+                complain("Error purchasing. Authenticity verification failed.");
+                setWaitScreen(false);
+                return;
+            }
+
+            Log.d(TAG, "Purchase successful.");
+
+            if (purchase.getSku().equals(SKU_R400)) {
+                Log.d(TAG, "Pagamento efetuado com sucesso. Volte sempre!");
+                alert("Pagamento efetuado com sucesso. Volte sempre!");
+            }
+           
+        }
+    };
+
+    // Called when consumption is complete
+  /*  IabHelper.OnConsumeFinishedListener mConsumeFinishedListener = new IabHelper.OnConsumeFinishedListener() {
+        public void onConsumeFinished(Purchase purchase, IabResult result) {
+            Log.d(TAG, "Consumption finished. Purchase: " + purchase + ", result: " + result);
+
+            // We know this is the "gas" sku because it's the only one we consume,
+            // so we don't check which sku was consumed. If you have more than one
+            // sku, you probably should check...
+            if (result.isSuccess()) {
+                // successfully consumed, so we apply the effects of the item in our
+                // game world's logic, which in our case means filling the gas tank a bit
+                Log.d(TAG, "Consumption successful. Provisioning.");
+                mTank = mTank == TANK_MAX ? TANK_MAX : mTank + 1;
+                saveData();
+                alert("You filled 1/4 tank. Your tank is now " + String.valueOf(mTank) + "/4 full!");
+            }
+            else {
+                complain("Error while consuming: " + result);
+            }
+            setWaitScreen(false);
+            Log.d(TAG, "End consumption flow.");
+        }
+    };
+*/
+    // Drive button clicked. Burn gas!
+    /*public void onDriveButtonClicked(View arg0) {
+        Log.d(TAG, "Drive button clicked.");
+        if (!mSubscribedToInfiniteGas && mTank <= 0) alert("Oh, no! You are out of gas! Try buying some!");
+        else {
+            if (!mSubscribedToInfiniteGas) --mTank;
+            saveData();
+            alert("Vroooom, you drove a few miles.");
+            updateUi();
+            Log.d(TAG, "Vrooom. Tank is now " + mTank);
+            
+            hernani 9635 8811
+            
+        }
+    }*/
+    
+    // We're being destroyed. It's important to dispose of the helper here!
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        
+        // very important:
+        Log.d(TAG, "Destroying helper.");
+        if (mHelper != null) mHelper.dispose();
+        mHelper = null;
+    }
+
+    // updates UI to reflect model
+    /*public void updateUi() {
+        // update the car color to reflect premium status or lack thereof
+        ((ImageView)findViewById(R.id.free_or_premium)).setImageResource(mIsPremium ? R.drawable.premium : R.drawable.free);
+
+        // "Upgrade" button is only visible if the user is not premium
+        findViewById(R.id.upgrade_button).setVisibility(mIsPremium ? View.GONE : View.VISIBLE);
+
+        // "Get infinite gas" button is only visible if the user is not subscribed yet
+        findViewById(R.id.infinite_gas_button).setVisibility(mSubscribedToInfiniteGas ? 
+                View.GONE : View.VISIBLE);
+
+        // update gas gauge to reflect tank status
+        if (mSubscribedToInfiniteGas) {
+            ((ImageView)findViewById(R.id.gas_gauge)).setImageResource(R.drawable.gas_inf);
+        }
+        else {
+            int index = mTank >= TANK_RES_IDS.length ? TANK_RES_IDS.length - 1 : mTank;
+            ((ImageView)findViewById(R.id.gas_gauge)).setImageResource(TANK_RES_IDS[index]);
+        }        
+    }
+*/
+    // Enables or disables the "please wait" screen.
+    void setWaitScreen(boolean set) {
+        findViewById(R.id.screen_main).setVisibility(set ? View.GONE : View.VISIBLE);
+        findViewById(R.id.screen_wait).setVisibility(set ? View.VISIBLE : View.GONE);
+    }
+
+    void complain(String message) {
+        Log.e(TAG, "**** One Touch Error: " + message);
+       // alert("Error: " + message);
+    }
+
+    void alert(String message) {
+        AlertDialog.Builder bld = new AlertDialog.Builder(this);
+        bld.setMessage(message);
+        bld.setNeutralButton("OK", null);
+        Log.d(TAG, "Showing alert dialog: " + message);
+        bld.create().show();
+    }
+
+}
